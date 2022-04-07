224fdc145fd4f46cd5ddfa353d3f0b3a18a16a52
icarus8050 <icarus8050@naver.com>
2022-04-07 12:23:49 +0900

    init commit

---
+++ src/main/kotlin/com/study/effectivekotlinreadingstudy/EffectiveKotlinReadingStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.study.effectivekotlinreadingstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EffectiveKotlinReadingStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<EffectiveKotlinReadingStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/effectivekotlinreadingstudy/EffectiveKotlinReadingStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.effectivekotlinreadingstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EffectiveKotlinReadingStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
